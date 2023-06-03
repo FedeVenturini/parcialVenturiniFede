e94cd55996c5aa22f537ebc4e02c06b34e8e9865
FedeVenturini <fedeventurini@gmail.com>
2023-06-02 21:44:09 -0300

    Form con validaciones

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import React from 'react'
 import Card from './components/card/Card'
+import Formulario from './components/formulario/Formulario'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Card />
+      <Formulario />
     </>
   )
 }
---
+++ src/components/formulario/Formulario.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import style from "./Formulario.module.css";
+
+const Formulario = () => {
+  const [nombre, setNombre] = useState("");
+  const [juego, setJuego] = useState("");
+  const [err, setErr] = useState("");
+
+  const onChangeNombre = (e) => {
+    setNombre(e.target.value);
+  };
+
+  const onChangeJuego = (e) => {
+    setJuego(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //expresión regular para asegurarme que no hay espacios blanco al principio
+    //obvio brindada por chatGpt
+    if (!/^\S/.test(nombre) || !/^\S/.test(juego)) {
+      setErr("Por favor chequea que la información sea correcta");
+    } else if (nombre.length <= 2 || juego.length <= 2) {
+      setErr("Por favor chequea que la información sea correcta");
+    } else {
+      setErr("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={style.form}>
+      <label htmlFor="nombre">Nombre: </label>
+      <input type="text" id="nombre" value={nombre} onChange={onChangeNombre} />
+
+      <label htmlFor="juego" className={style.label}>
+        Videojuego favorito:{" "}
+      </label>
+      <input type="text" id="juego" value={juego} onChange={onChangeJuego} />
+      <button>Enviar info 🕹️</button>
+      {err && <p>{err}</p>}
+    </form>
+  );
+};
+
+export default Formulario;
